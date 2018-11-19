2e2e863570a43c2584d892934deb5e5e3734af88
Questwalker <44627006+Questwalker@users.noreply.github.com>
2018-11-18 19:20:04 -0700

    An autoclicker!
    Put in what you want it to type,
    Tell it if you want it to constantly loop,
    Give it a delay to go in between each key input,
    And, if its going to loop, tell it what key should stop it from looping when you press it.
    
    But, you MUST have python's pip installed: https://www.youtube.com/watch?v=cm6WDGAzDPM in order for it to work!

---
+++ AutoClicker.py
@@ -0,0 +1,90 @@
+from pynput.keyboard import Key, Controller
+import time
+import re
+import sys
+import os
+import keyboard
+i=0
+pause = False
+
+### START ###
+time.sleep(3)
+
+#Run Information Query
+print ("Keypresser by Questwalker")
+print ('\n')
+keys = input ("What keys do you want to press? ")
+loop = input ("Loop?(Y/N) ")
+delay = input ("Delay the keypresses by? (In seconds) ")
+keypresses = list(keys)
+print ('\n')
+
+#"Loop" variable Must Be True or False (Y/N)
+while not loop == True:
+    if loop == "Y":
+        loop = True
+        break 
+    elif loop == "y":
+        loop = True
+        break 
+    elif loop == "N":
+        loop = False
+        break 
+    elif loop == "n":
+        loop = False
+        break 
+    else:
+        loop = input ("Please enter a valid Loop.(Y/N) ")
+    if loop == False:
+        break
+print("")
+
+#Stop Loop Shortcut
+if loop == True:
+    pause = input("What key should be be used to stop this program? ")
+
+#Remove Any Extra Numbers From "Delay" Variable
+re.sub('[abcdefghijklmnopqrstuvwxyz]', '', delay)
+delay = re.sub('[abcdefghijklmnopqrstuvwxyz]', '', delay)
+
+#Countdown to Start Program
+print ("Starting in.. 5", end="\r")
+time.sleep(1)
+print ("Starting in.. 4", end="\r")
+time.sleep(1)
+print ("Starting in.. 3", end="\r")
+time.sleep(1)
+print ("Starting in.. 2", end="\r")
+time.sleep(1)
+print ("Starting in.. 1")
+time.sleep(1)
+
+delay = float(delay)
+
+#Loop and exit Scripts
+if loop == True:
+    print ("Close this program tab to leave")
+    while 1 == 1:
+        if i > len(keypresses)-1:
+            i = 0
+            time.sleep(1)
+
+        keyboard.press(keypresses[i])
+        keyboard.release(keypresses[i])
+        i += 1
+        time.sleep(delay)
+
+        if keyboard.is_pressed(pause): 
+            break
+
+
+if loop == False:
+    while not i == len(keypresses):
+
+        keyboard.press(keypresses[i])
+        keyboard.release(keypresses[i])
+        i += 1
+        time.sleep(delay)
+    
+print ("Done!")
+quit ()
